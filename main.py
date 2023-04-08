980f548f326c89e21a8a3129940e0bd37f103950
JulianLopezB <julianlopezb@gmail.com>
2023-04-08 16:34:16 +0200

    new retrieval method

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import torch.nn.utils.rnn as r
 import cloudpickle as cpickle
 import torch
 from flask import Flask, jsonify, request, make_response
-import requests
 from flask_cors import CORS
+import numpy as np
+from tqdm import tqdm
+import pickle
+import io
+
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
+
 
 app = Flask(__name__)
 CORS(app)
@@ -16,35 +27,30 @@ input_resolution = model.visual.input_resolution
 context_length = model.context_length
 vocab_size = model.vocab_size
 
-with open('./data/mango_embeddings_small_cpu.pkl', 'rb') as f:
-    e_mango = cpickle.load(f)
-    
-with open('./data/zara_embeddings_small_cpu.pkl', 'rb') as f:
-    e_zara = cpickle.load(f)
-    
-with open('./data/vogue_embeddings.pkl', 'rb') as f:
-    e_vogue = cpickle.load(f)
-    
-mango_df = pd.read_csv('./data/mango_small.csv', index_col=[0])
-zara_df = pd.read_csv('./data/zara_small.csv', index_col=[0])
-vogue_df = pd.read_csv('./data/vogue.csv', index_col=[0])
+blacklist = ['https://www.stradivarius.com/es/coletero-grande-saten-l00150411']
 
-BLACKLIST = ['SUDADERA MUÑECO DE NIEVE']
+images_df = pd.read_csv('./data/all_brands.csv', index_col=[0]).set_index('img_url')
 
-all_data = {
-    'mango': {
-        'images': mango_df,
-        'embeddings': e_mango
-        },
-    'zara': {
-        'images': zara_df,
-        'embeddings': e_zara
-        },
-    'vogue': {
-        'images': vogue_df,
-        'embeddings': e_vogue
-        }
-    }
+blacklist_img_urls = images_df[images_df['shop_link'].isin(blacklist)]
+# images_df = images_df[~images_df['shop_link'].isin(blacklist)]
+
+with open('./data/all_images.pkl', 'rb') as f:
+    # e_img = cpickle.load(f)
+    e_img = CPU_Unpickler(f).load()
+    
+e_img = {k: v for k,v in e_img.items() if v is not None}
+
+e_img_cat = torch.cat(list(e_img.values()))
+e_img_df = pd.DataFrame(e_img_cat, index=e_img.keys())
+
+templates = [
+    '{}.',
+    # 'a photo of a {}.',
+    'a person wearing {}.',
+    '{} style.',
+    '{} fashion.',
+    '{} inspiration.'
+             ]
 
 
 def compute_text_embeddings(text):
@@ -56,39 +62,82 @@ def compute_text_embeddings(text):
 def get_similarity(image_features, text_features):
     image_features /= image_features.norm(dim=-1, keepdim=True)
     text_features /= text_features.norm(dim=-1, keepdim=True)
-    similarity = text_features.cpu().numpy() @ image_features.cpu().numpy().T
+    similarity = text_features.cpu() @ image_features.cpu().T
     return similarity
 
-def retrieve_most_similar_images(prompt, max_k=30, threshold = 0.17):
+def retrieve_most_similar_images(prompt, max_k=100, threshold = 0.2):
+    prompt = prompt.lower().replace('style', '')
+    prompt = prompt.replace('fashion', '')
+    prompt = prompt.replace('inspiration', '')
+    prompt =prompt.lower()
+    texts = [template.format(prompt) for template in templates] 
+    e_text_cat = torch.cat([compute_text_embeddings(t) for t in texts]).to('cpu')
     
-    e_t = compute_text_embeddings(prompt)
+    similarity = get_similarity(e_img_cat, e_text_cat)
+    top_idx = np.array(list(set(similarity.topk(max_k).indices.ravel().tolist())))
+    top_idx = top_idx[(similarity[:, top_idx].max(0).values>threshold).numpy()]
     
     similar_items = []
-    for source, data in all_data.items():
-        e = data['embeddings']
-        images_df = data['images']
-        for k in e:
-            if e[k] is not None:
-                similarity = get_similarity(e[k], e_t)[0][0]
-                if similarity >= threshold:
-                    item_data = {}
-                    item_data['similarity'] = similarity
-                    item_data['brand'] = source
-                    item_data['name'] = images_df.loc[k].get('name')
-                    item_data['category'] = images_df.loc[k].get('category')
-                    item_data['img_urls'] = images_df.loc[k].get('img_urls')
-                    
-                    if item_data['name'] not in BLACKLIST:
-                        similar_items.append(item_data)
-                    
-    return pd.DataFrame(similar_items).sort_values(by='similarity', ascending=False).head(max_k).to_dict('records')
+    for idx in top_idx:
+        idx = (idx.item())
+        idx_ = e_img_df.index[idx]
+        if idx_ not in blacklist_img_urls:
+            item_data = {}
+            item_data['similarity'] = similarity[:, idx].max().item()
+            item_data['brand'] = images_df.loc[idx_]['brand']
+            item_data['name'] = images_df.loc[idx_].get('name')
+            item_data['category'] = images_df.loc[idx_].get('section')
+            item_data['img_urls'] = idx_
+            item_data['price'] = images_df.loc[idx_].get('price')
+            item_data['price_float'] = images_df.loc[idx_].get('price_float')
+            item_data['old_price'] = images_df.loc[idx_].get('old_price')
+            item_data['old_price_float'] = images_df.loc[idx_].get('old_price_float')
+            item_data['discount_rate'] = images_df.loc[idx_].get('discount_rate')
+            item_data['sale'] = images_df.loc[idx_].get('sale')
+            item_data['shop_link'] = images_df.loc[idx_].get('shop_link')
+            similar_items.append(item_data)
+        
+    if len(similar_items)>0:
+        similar_items = pd.DataFrame(similar_items)
+        similar_items = similar_items.sort_values(by='similarity', ascending=False)
+        similar_items = similar_items.drop_duplicates(subset=['img_urls'], keep='first')
+        if max_k is not None:
+            similar_items = similar_items.head(max_k)
+        similar_items = similar_items.to_dict('records')
+   
+    return similar_items
 
 @app.route("/api/v1/search", methods=["GET"])
 def index():
-    text = request.args.get('query')
-    if text is None:
+    query = request.args.get('query')
+    section = request.args.get('section')
+    on_sale = request.args.get("on_sale", default=False, type=bool)
+    brand = request.args.get('brand')
+    
+    if all([i is None for i in [query, section, on_sale, brand]]):
         make_response(jsonify({'error': 'Missing text parameter'}), 400)
-    results = retrieve_most_similar_images(text)
+    
+    
+    else:
+        results = []
+        if query is not None:
+            query_results = retrieve_most_similar_images(query)
+            
+            for item in query_results:
+                results.append(item)
+        if section is not None:
+            filtered = images_df[images_df['section']==section].to_dict('records')
+            for item in filtered:
+                results.append(item)
+        if brand is not None:
+            filtered = images_df[images_df['brand']==brand].to_dict('records')
+            for item in filtered:
+                results.append(item)
+        if on_sale is not False:
+            print(on_sale)
+            filtered = images_df[images_df['sale']==on_sale].to_dict('records')
+            for item in filtered:
+                results.append(item)
 
     return jsonify(results)
 

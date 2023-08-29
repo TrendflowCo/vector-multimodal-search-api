bd95b993c15e1706f976902420996b139a2acaa1
Julian López <julian@MacBook-Pro-de-Julian.local>
2023-08-29 22:53:43 +0200

    Adding new endpoints and data integration

--- gcp_utils.py
+++ gcp_utils.py
@@ -9,9 +9,9 @@ storage_client = storage.Client()
 bucket = storage_client.bucket('fashion-embeddings')
 
 # blobs
-all_images_blob = bucket.blob('all_images.pkl')
-all_images_fclip_blob = bucket.blob('all_images_fclip.pkl')
-tags_embeddings_blob = bucket.blob('tags_embeddings.pkl')
+# all_images_blob = bucket.blob('all_images.pkl')
+# all_images_fclip_blob = bucket.blob('all_images_fclip.pkl')
+# tags_embeddings_blob = bucket.blob('tags_embeddings.pkl')
 
 def load_data_from_blob(blob):
     with blob.open("rb") as f:
@@ -21,15 +21,3 @@ def save_data_to_blob(blob, data):
     with blob.open("wb") as f:
         pkl.dump(data, f)
 
-
-# bq functions obtained from https://blog.coupler.io/how-to-crud-bigquery-with-python/
-
-def query_datasets_to_df(QUERY):
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    client = bigquery.Client()
-    
-    query_job = client.query(QUERY)
-    
-    df = query_job.to_dataframe()
-    return df
--- utils.py
+++ utils.py
@@ -4,3 +4,7 @@ def translate(text, source_language):
     translator = Translator()
     translation = translator.translate(text, src=source_language, dest='en')
     return translation.text
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()

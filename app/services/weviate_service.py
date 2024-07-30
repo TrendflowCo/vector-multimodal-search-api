3d852b2a4b8f6650c9f9e592d5c425100b04d018
Julian López <julian@MacBook-Pro-de-Julian.local>
2024-07-30 11:16:00 +0200

    Several improvements- env vars, total results,..

--- app/services/weviate_service.py
+++ app/services/weviate_service.py
@@ -2,7 +2,7 @@ import weaviate
 from weaviate.auth import AuthApiKey
 from app.services.text_service import TextService
 from app.common.exceptions import DataNotFoundError, InvalidParameterError, ServiceUnavailableError, ProcessingError, AuthenticationError, AuthorizationError
-from app.config import WEVIATE_CLASS_NAME, PROPERTIES  # Import the WEVIATE_CLASS_NAME at the top of the file
+from app.config import WEAVIATE_CLASS_NAME, PROPERTIES
 import os
 
 class WeaviateService:
@@ -10,10 +10,11 @@ class WeaviateService:
         self.client = self.init_client()
         self.text_service = TextService()  # Initialize TextService here
         self.properties = PROPERTIES
+        self.weaviate_class_name = WEAVIATE_CLASS_NAME
         
     def init_client(self):
-        URL = os.environ.get('WEAVIATE_URL')
-        APIKEY = os.environ.get('WEAVIATE_API_KEY')
+        URL = os.getenv('WEAVIATE_URL')
+        APIKEY = os.getenv('WEAVIATE_API_KEY')
         try:
             client = weaviate.Client(
                 url=URL,
@@ -28,10 +29,10 @@ class WeaviateService:
     def add_data(self, data):
         if not isinstance(data, dict):
             raise InvalidParameterError("Data must be a dictionary")
-        self.client.data_object.create(data, WEVIATE_CLASS_NAME)
+        self.client.data_object.create(data, self.weaviate_class_name)
 
     def query_data(self, filters=None, limit=100):
-        query_builder = self.client.query.get(WEVIATE_CLASS_NAME, self.properties)
+        query_builder = self.client.query.get(self.weaviate_class_name, self.properties)
         if filters:
             query_builder = query_builder.with_where(filters)
         query_builder = query_builder.with_limit(limit)
@@ -41,46 +42,61 @@ class WeaviateService:
             raise ProcessingError(f"Failed to process query: {str(e)}")
 
     def search_with_text(self, query_text, threshold, search_type="bm25", filters=None, limit=10, offset=0, sort_by=None, ascending=True):
-        query_builder = self.client.query.get(WEVIATE_CLASS_NAME, self.properties)
+        query_builder = self.client.query.get(self.weaviate_class_name, self.properties)
+        count_query = self.client.query.aggregate(self.weaviate_class_name).with_meta_count()
+
         if search_type == "bm25":
             query_builder = query_builder.with_bm25(query=query_text)
+            count_query = count_query.with_bm25(query=query_text)
         elif search_type == "clip":
-            # Using get_text_embeddings from TextService
             query_vector = self.text_service.get_text_embeddings(query_text)
-            query_builder = query_builder.with_near_vector({
+            vector_search = {
                 'vector': query_vector,
-                'distance': threshold,  # max accepted distance
-                })
+                'distance': threshold,
+            }
+            query_builder = query_builder.with_near_vector(vector_search)
+            count_query = count_query.with_near_vector(vector_search)
         elif search_type == "hybrid":
-            # Using get_text_embeddings from TextService
             query_vector = self.text_service.get_text_embeddings(query_text)
-            query_builder = query_builder.hybrid({
+            hybrid_search = {
                 'query': query_text,
                 'vector': query_vector,
                 'alpha': 0.25,
-                'distance': threshold,  # max accepted distance
-            })
+                'distance': threshold,
+            }
+            query_builder = query_builder.hybrid(hybrid_search)
+            count_query = count_query.hybrid(hybrid_search)
             
         query_builder = query_builder.\
                             with_limit(limit).\
                             with_offset(offset).\
-                            with_additional('distance')
+                            with_additional(['id', 'distance'])
 
         if filters:
-            query_builder = query_builder.with_where({
-                "operator": "And", 
+            filter_condition = {
+                "operator": "And",
                 "operands": filters
-            })
+            }
+            query_builder = query_builder.with_where(filter_condition)
+            count_query = count_query.with_where(filter_condition)
 
         if sort_by:
             order = 'asc' if ascending else 'desc'
             query_builder = query_builder.with_sort({
                 'path': [sort_by], 
-                'order': order}
-                )
+                'order': order
+            })
 
         try:
-            return query_builder.do()
+            results = query_builder.do()            
+            items = results.get('data', {}).get('Get', {}).get(self.weaviate_class_name, [])
+            
+            # count_result = count_query.do()
+            # total_results = count_result['data']['Aggregate'][self.weaviate_class_name][0]['meta']['count']
+            total_results = len(items)
+
+            return items, total_results
+
         except Exception as e:
             raise ProcessingError(f"Failed to process query: {str(e)}")
 
@@ -91,7 +107,7 @@ class WeaviateService:
         ]
 
         result = self.client.query.get(
-            WEVIATE_CLASS_NAME, self.properties
+            self.weaviate_class_name, self.properties
         ).with_where({
             "operator": "And",
             "operands": operands
@@ -101,7 +117,7 @@ class WeaviateService:
 
     def get_similar_items(self, product_id, top_k=20, sort_by=None, ascending=True, filters=None):
         product_vector = self.client.query.get(
-            WEVIATE_CLASS_NAME, ["_additional { vector }"]
+            self.weaviate_class_name, ["_additional { vector }"]
         ).with_where({
             "path": ["id_item"],
             "operator": "Equal",
@@ -110,13 +126,13 @@ class WeaviateService:
         
         if 'errors' in product_vector:
             raise ProcessingError(f"Query failed: {product_vector['errors']}")
-        if not product_vector.get('data', {}).get('Get', {}).get(WEVIATE_CLASS_NAME):
+        if not product_vector.get('data', {}).get('Get', {}).get(self.weaviate_class_name):
             raise DataNotFoundError('Product not found or no data returned')
 
-        vector = product_vector['data']['Get'][WEVIATE_CLASS_NAME][0]['_additional']['vector']
+        vector = product_vector['data']['Get'][self.weaviate_class_name][0]['_additional']['vector']
 
         query_builder = self.client.query.get(
-            WEVIATE_CLASS_NAME, self.properties
+            self.weaviate_class_name, self.properties
         ).with_near_vector({
             "vector": vector
         }).with_limit(top_k)
@@ -137,20 +153,22 @@ class WeaviateService:
         return query_builder.do()
 
     def get_all_brands(self):
-        # Use WEVIATE_CLASS_NAME directly
-        result = self.client.query.aggregate(
-            WEVIATE_CLASS_NAME
-        ).with_where({
-            "path": ["country"],
-            "operator": "Equal",
-            "valueString": 'es'
-        }).with_fields(
-            "groupedBy { value }"
-        ).with_group_by_filter(
-            "brand"
-        ).do()
-        
-        if 'data' in result and 'Aggregate' in result['data']:
-            brands = [item['groupedBy']['value'] for item in result['data']['Aggregate'][WEVIATE_CLASS_NAME]]
-            return brands
-        return []
+        try:
+            # Define the filter and group by criteria for entries where country is 'es'
+            group_by_filter_criteria = {
+                "operator": "Equal",
+                "path": ["country"],
+                "valueString": "es"
+            }
+
+            response = (
+                self.client.query
+                .aggregate(self.weaviate_class_name)
+                .with_group_by_filter(group_by_filter_criteria) 
+                .with_meta_count()
+                .with_fields("meta { count } groupedBy { path value }")
+                .do()
+            )
+            return [item['value'] for item in response['data']['Aggregate'][self.weaviate_class_name]['groupedBy']]
+        except Exception as e:
+            raise ProcessingError(f"Failed to retrieve brands: {str(e)}")
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+# import os
+
+# Manually load the .env file
+# with open('.env', 'r') as file:
+#     for line in file:
+#         if line.startswith('#') or line.strip() == '':
+#             continue
+#         try:    
+#             key, value = line.strip().split('=', 1)
+#             os.environ[key] = value
+#         except ValueError:
+#             print(f"Invalid line in .env file: {line}")
+
 from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
+
+load_dotenv('.env', override=True)
 
 from app.api.endpoints import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
     # Run the Flask application
-    import os
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.getenv("PORT", 8080))
     app.run(host='0.0.0.0', port=port)

c76a8f13d7984e9c6d02f2a53b6366f2217d707a
Julian Lopez Baasch <julianlopezba@gmail.com>
2024-10-16 22:32:24 +0200

    Cleaning main script

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# import os
-
-# Manually load the .env file
-# with open('.env', 'r') as file:
-#     for line in file:
-#         if line.startswith('#') or line.strip() == '':
-#             continue
-#         try:    
-#             key, value = line.strip().split('=', 1)
-#             os.environ[key] = value
-#         except ValueError:
-#             print(f"Invalid line in .env file: {line}")
 
 from dotenv import load_dotenv
 
@@ -23,4 +11,4 @@ app = create_app()
 if __name__ == "__main__":
     # Run the Flask application
     port = int(os.getenv("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)

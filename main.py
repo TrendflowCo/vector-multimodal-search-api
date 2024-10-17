27ec63a979402b0dcdd778cf700466e8f7235a01
Lopez Baasch, Julian <julian.lopez@stefanini.com>
2024-10-17 18:34:32 +0200

    Adding api key for security

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-
 from dotenv import load_dotenv
 
 load_dotenv('.env', override=True)
 
 from app.api.endpoints import create_app
 import os
+from app.security import limiter
 
 app = create_app()
+limiter.init_app(app)
 
 if __name__ == "__main__":
     # Run the Flask application
